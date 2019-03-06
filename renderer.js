484e3770f8f9137b66ab922073d644e205513c03
Tobias Wältken <tobias.w@ltken.de>
2019-03-06 14:00:39 +0100

    Add KV Store and add a main config

--- renderer.js
+++ renderer.js
@@ -1,3 +1,11 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+const KVStore = require('./kv-store.js');
+const config = new KVStore({
+	name: 'preferences',
+	defaults: {}
+});
+
+console.log(config.get('window'));
